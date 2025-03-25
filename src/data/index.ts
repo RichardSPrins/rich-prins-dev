fc3651b548b1cd63a802c52091ed77ad499b7a87
Richard Prins <richardprins@Richards-Mini.lan>
2025-03-25 14:39:08 -0600

    filling out template

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,84 +1,83 @@
-export const SITE_TITLE = "Sanju's Space";
-export const SITE_DESCRIPTION = "A internet space for Sanju.";
+export const SITE_TITLE = "Rich's Space";
+export const SITE_DESCRIPTION = "A internet space for Rich.";
 
 export interface MenuItem {
-	label: string;
-	url: string;
+  label: string;
+  url: string;
 }
 
 export const menuItems: MenuItem[] = [
-	{
-		label: "home",
-		url: "/",
-	},
-	{
-		label: "writings",
-		url: "/writings",
-	},
-	{
-		label: "thoughts",
-		url: "/thoughts",
-	},
-	{
-		label: "ships",
-		url: "/ships",
-	},
+  {
+    label: "home",
+    url: "/",
+  },
+  {
+    label: "blog",
+    url: "/writings",
+  },
+  {
+    label: "thoughts",
+    url: "/thoughts",
+  },
+  {
+    label: "projects",
+    url: "/projects",
+  },
 ];
 
-export const title = "Sanju's Space";
-export const description = "A internet space for Sanju.";
+export const title = "Rich's Space";
+export const description = "A internet space for Rich.";
 export const image = "/images/ogimage.png";
-export const url = "https://sanju.sh";
+export const url = "https://richprins.dev";
 
 export const ogImage = {
-	src: "/images/ogimage.png",
-	alt: "Sanju's Space",
+  src: "/images/ogimage.png",
+  alt: "Rich's Space",
 };
 
-
 export const products = [
-	{
-		name: "Fli.so",
-		url: "https://fli.so",
-		image: "/products/fli.png",
-	},
-	{
-		name: "Uiino",
-		url: "https://uiino.com",
-		image: "/products/uiino.png",
-	},
-	{
-		name: "SticAI",
-		url: "https://sticai.com",
-		image: "/products/sticai.png",
-	},
-	{
-		name: "Dun",
-		url: "https://dunsuite.com",
-		image: "/products/dun.png",
-	},
-	{
-		name: "DunTasks",
-		url: "https://duntasks.com",
-		image: "/products/duntasks.png",
-	},
+  {
+    name: "Fli.so",
+    url: "https://fli.so",
+    image: "/products/fli.png",
+  },
+  {
+    name: "Uiino",
+    url: "https://uiino.com",
+    image: "/products/uiino.png",
+  },
+  {
+    name: "SticAI",
+    url: "https://sticai.com",
+    image: "/products/sticai.png",
+  },
+  {
+    name: "Dun",
+    url: "https://dunsuite.com",
+    image: "/products/dun.png",
+  },
+  {
+    name: "DunTasks",
+    url: "https://duntasks.com",
+    image: "/products/duntasks.png",
+  },
 ];
 
 export const socialLinks = [
-	{
-		label: "@x",
-		url: "https://x.com/spikeysanju",
-	},
-	{
-		label: "email",
-		url: "mailto:work@sanju.sh",
-	},
-	{
-		label: "github",
-		url: "https://github.com/spikeysanju",
-	},
-	{
-		label: "linkedin",
-		url: "https://www.linkedin.com/in/imsanju",
-	},
+  {
+    label: "@x",
+    url: "https://x.com/richprins_dev",
+  },
+  {
+    label: "email",
+    url: "mailto:hello@extensiblmedia.com",
+  },
+  {
+    label: "github",
+    url: "https://github.com/RichardSPrins",
+  },
+  {
+    label: "linkedin",
+    url: "https://www.linkedin.com/in/richard-prins-jr",
+  },
 ];
